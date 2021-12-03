6344a6e44e43c657954fe68ec698f5bc355ae7ed
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-04 00:12:19 +0800

    Settings: Make QS footer icons optional [2/2]
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

---
+++ src/com/android/settings/display/QsFooterIconsSettings.java
@@ -0,0 +1,27 @@
+package com.android.settings.display;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class QsFooterIconsSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        addPreferencesFromResource(R.xml.qs_footer_icons);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+}
