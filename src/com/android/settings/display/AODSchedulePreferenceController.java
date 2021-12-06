6aee90b4e847210b02dbb9f28044fb263081b35b
Ido Ben-Hur <idoybh2@gmail.com>
2021-12-06 16:09:18 +0800

    AODSchedule: Add support for mixed time & sun modes [2/2]
    Also make the code a bit more readable
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- src/com/android/settings/display/AODSchedulePreferenceController.java
+++ src/com/android/settings/display/AODSchedulePreferenceController.java
@@ -11,6 +11,12 @@ public class AODSchedulePreferenceController extends BasePreferenceController {
 
     private Context mContext;
 
+    static final int MODE_DISABLED = 0;
+    static final int MODE_NIGHT = 1;
+    static final int MODE_TIME = 2;
+    static final int MODE_MIXED_SUNSET = 3;
+    static final int MODE_MIXED_SUNRISE = 4;
+
     public AODSchedulePreferenceController(Context context, String key) {
         super(context, key);
         mContext = context;
@@ -26,12 +32,17 @@ public class AODSchedulePreferenceController extends BasePreferenceController {
         final int mode = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
         switch (mode) {
-            case 0:
+            default:
+            case MODE_DISABLED:
                 return mContext.getResources().getString(R.string.disabled);
-            case 1:
+            case MODE_NIGHT:
                 return mContext.getResources().getString(R.string.night_display_auto_mode_twilight);
-            case 2:
+            case MODE_TIME:
                 return mContext.getResources().getString(R.string.night_display_auto_mode_custom);
+            case MODE_MIXED_SUNSET:
+                return mContext.getResources().getString(R.string.always_on_display_schedule_mixed_sunset);
+            case MODE_MIXED_SUNRISE:
+                return mContext.getResources().getString(R.string.always_on_display_schedule_mixed_sunrise);
         }
     }
 }
