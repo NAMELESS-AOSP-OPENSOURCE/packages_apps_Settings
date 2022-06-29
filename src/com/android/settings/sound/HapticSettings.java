817c5f08f473971fd38a5129cde8bf4aad89698f
cjh1249131356 <cjh1249131356@gmail.com>
2022-06-29 22:42:51 +0800

    Settings: Contionally enable some haptic feedback option
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- src/com/android/settings/sound/HapticSettings.java
+++ src/com/android/settings/sound/HapticSettings.java
@@ -1,20 +1,49 @@
+/*
+ * Copyright (C) 2022 The Nameless-AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package com.android.settings.sound;
 
 import android.os.Bundle;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.custom.CustomUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.custom.preference.SystemSettingSwitchPreference;
 
 public class HapticSettings extends SettingsPreferenceFragment {
+
+    private static final String KEY_HAPTIC_ON_SLIDER = "haptic_on_slider";
+    private static final String KEY_HAPTIC_ON_MISC = "haptic_on_misc";
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         addPreferencesFromResource(R.xml.haptic_settings);
+
+        SystemSettingSwitchPreference slider = (SystemSettingSwitchPreference) findPreference(KEY_HAPTIC_ON_SLIDER);
+        SystemSettingSwitchPreference misc = (SystemSettingSwitchPreference) findPreference(KEY_HAPTIC_ON_MISC);
+
+        if (!CustomUtils.hasLinearMotorVibrator(getContext())) {
+            final PreferenceScreen prefSet = getPreferenceScreen();
+            prefSet.removePreference(slider);
+            prefSet.removePreference(misc);
+        }
     }
 
     @Override
--- src/com/android/settings/widget/SeekBarPreference.java
+++ src/com/android/settings/widget/SeekBarPreference.java
@@ -36,6 +36,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import androidx.core.content.res.TypedArrayUtils;
 import androidx.preference.PreferenceViewHolder;
 
+import com.android.internal.util.custom.CustomUtils;
 import com.android.internal.util.custom.VibratorHelper;
 
 import com.android.settingslib.RestrictedPreference;
@@ -68,6 +69,7 @@ public class SeekBarPreference extends RestrictedPreference
 
     private final Context mContext;
     private final VibratorHelper mVibratorHelper;
+    private final boolean mHasLinearMotorVibrator;
 
     public SeekBarPreference(
             Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -77,6 +79,7 @@ public class SeekBarPreference extends RestrictedPreference
         mVibratorHelper = new VibratorHelper(context,
                 Settings.System.HAPTIC_FEEDBACK_ENABLED,
                 Settings.System.HAPTIC_ON_SLIDER);
+        mHasLinearMotorVibrator = CustomUtils.hasLinearMotorVibrator(context);
 
         TypedArray a = context.obtainStyledAttributes(
                 attrs, com.android.internal.R.styleable.ProgressBar, defStyleAttr, defStyleRes);
@@ -301,17 +304,21 @@ public class SeekBarPreference extends RestrictedPreference
                 setProgress(progress, false);
                 switch (mHapticFeedbackMode) {
                     case HAPTIC_FEEDBACK_MODE_ON_TICKS:
-                        int duration = 100;
-                        if (progress != mMin && progress != mMax && mMin != mMax) {
-                            duration = (int) (1 + 48 * (progress - mMin) / (mMax - mMin));
+                        if (mHasLinearMotorVibrator) {
+                            int duration = 100;
+                            if (progress != mMin && progress != mMax && mMin != mMax) {
+                                duration = (int) (1 + 48 * (progress - mMin) / (mMax - mMin));
+                            }
+                            mVibratorHelper.vibrateForDuration(duration);
                         }
-                        mVibratorHelper.vibrateForDuration(duration);
                         break;
                     case HAPTIC_FEEDBACK_MODE_ON_ENDS:
-                        if (progress == mMax || progress == mMin) {
-                            if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                                    Settings.System.HAPTIC_ON_SLIDER, 1, UserHandle.USER_CURRENT) == 1) {
-                                seekBar.performHapticFeedback(CLOCK_TICK);
+                        if (mHasLinearMotorVibrator) {
+                            if (progress == mMax || progress == mMin) {
+                                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                                        Settings.System.HAPTIC_ON_SLIDER, 1, UserHandle.USER_CURRENT) == 1) {
+                                    seekBar.performHapticFeedback(CLOCK_TICK);
+                                }
                             }
                         }
                         break;
